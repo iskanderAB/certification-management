49f30f9a3eb0bb7fb2c6052211a1fa621fdefdb0
iskanderAB <iskanderabasi@gmail.com>
2022-04-06 03:22:01 +0100

    calander

--- src/Controller/SalaryCertificateController.php
+++ src/Controller/SalaryCertificateController.php
@@ -9,7 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Dompdf\Dompdf;
+use Dompdf\Options;
 /**
  * @Route("/salary/certificate")
  */
@@ -50,6 +51,7 @@ class SalaryCertificateController extends AbstractController
      */
     public function show(SalaryCertificate $salaryCertificate): Response
     {
+        $this->generatePdf($salaryCertificate);
         return $this->render('salary_certificate/show.html.twig', [
             'salary_certificate' => $salaryCertificate,
         ]);
@@ -84,5 +86,35 @@ class SalaryCertificateController extends AbstractController
         }
 
         return $this->redirectToRoute('app_salary_certificate_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    private function generatePdf($salaryCertificate)
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('salary_certificate/show.html.twig', [
+            'salary_certificate' => $salaryCertificate,
+        ]);
+        $dompdf->set_option('isRemoteEnabled',TRUE);
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("attestation_".$salaryCertificate->getId().".pdf", [
+            "Attachment" => false
+        ]);
+        exit();
     }
 }
