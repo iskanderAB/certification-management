4e8467acfce2c1f13308070d60e532b0f49a19f4
iskanderAB <iskanderabasi@gmail.com>
2022-04-27 10:51:57 +0100

    fix salary date

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    /**
+     * @Route("/dashboard", name="app_dashboard")
+    */
     public function index(): Response
     {
         return $this->render('dashboard/index.html.twig', [
